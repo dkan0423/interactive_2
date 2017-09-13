6758f6e010f3fa3117ffe82f56af6162b3c18326
dkan0423 <denisekan@gmail.com>
2017-09-12 21:14:01 -0700

    sdfds

---
+++ 50buttons/assets/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	   
+
+    
+	// $('#button-4').click(function() {
+	// 	$('#button-3').animate({
+	// 		opacity: 0.4,
+	// 		marginLeft: '0.6in',
+	// 		fontSize: '24px',
+	// 		borderWidth: '10px'
+	// 		background-color: 'yellow'; 
+	// 	}, 1000 );
+	// });
+
+	// $('#button-3').mouseover(function() {
+	// 	$(this).animate({
+	// 		marginLeft: '1in'
+	// 	}, 1000 );
+
+});
